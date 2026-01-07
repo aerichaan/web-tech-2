f719c816f80f7d348717c5d51a5a4c12e11099c5
Joana Mae <jmbarbarona08317@liceo.edu.ph>
2026-01-07 13:01:09 +0800

    prelim-exam submission

---
+++ src/app/prelim-exam/prelim-exam.component.ts
@@ -0,0 +1,10 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-prelim-exam',
+  templateUrl: './prelim-exam.component.html',
+  styleUrls: ['./prelim-exam.component.scss']
+})
+export class PrelimExamComponent {
+  title = signal('Prelim Exam');
+}
